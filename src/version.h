5a2ff1ef9a46c28bbbd6a2c697bfef42f7e0336c
Tobias Rausch <rauschtobi@gmail.com>
2026-02-26 14:24:33 +0100

    v0.0.6

--- src/version.h
+++ src/version.h
@@ -5,7 +5,7 @@ namespace breaktracer
 {
 
 
-  std::string breaktracerVersionNumber = "0.0.5";
+  std::string breaktracerVersionNumber = "0.0.6";
 
   inline 
     void printTitle(std::string const& title) 
